d62bd711617d352ee79e98a10bc322bfaaa063c1
Clement Bowe <cbowe023@fiu.edu>
2021-06-24 15:25:07 -0400

    added react router and hook forms

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SplashPage from './pages/splashPage';
+
+
 
 function App() {
+  const [userToken, setUserToken] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplashPage/>
   );
 }
 
---
+++ my-app/src/components/loginForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginForm = () => {
+  
+    
+    const handleSubmit = ()=>{
+    }
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default LoginForm;
